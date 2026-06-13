d4447e7077f9eccc58bdf2b820a623d4bab2b6b2
Jeffser <jsamuel@tuta.io>
2026-06-13 07:19:05 -0600

    dynamic loading for starred songs

--- src/widgets/pages/songs_starred.py
+++ src/widgets/pages/songs_starred.py
@@ -3,7 +3,7 @@
 from gi.repository import Gtk, Adw, GLib
 from ...integrations import get_current_integration
 from ..song import SongRow, SongButton
-import re
+import re, threading
 
 @Gtk.Template(resource_path='/com/jeffser/Nocturne/pages/songs_starred.ui')
 class SongsStarredPage(Adw.NavigationPage):
@@ -11,29 +11,71 @@ class SongsStarredPage(Adw.NavigationPage):
 
     list_el = Gtk.Template.Child()
     wrapbox_el = Gtk.Template.Child()
+    search_entry_el = Gtk.Template.Child()
     main_stack = Gtk.Template.Child()
+    scrolledwindow = Gtk.Template.Child()
+    song_ids = []
+    offset = 0
+    searching = False
 
-    def reload(self):
-        GLib.idle_add(self.main_stack.set_visible_child_name, 'loading')
+    def __init__(self):
+        super().__init__()
+        self.scrolledwindow.get_vadjustment().connect('notify::upper', lambda va, ud: GLib.timeout_add(1000, self.check_scrollbar, va))
+
+    def search(self):
+        if self.searching:
+            return
+        self.searching = True
         integration = get_current_integration()
-        songs = integration.getStarredSongs()
-        GLib.idle_add(self.reset)
-        for id in songs:
-            GLib.idle_add(self.list_el.list_el.append, SongRow(id))
-            GLib.idle_add(self.wrapbox_el.append, SongButton(id))
+        ids_to_show = []
+        if query := self.search_entry_el.get_text():
+            for song_id in self.song_ids:
+                if model := integration.loaded_models.get(song_id):
+                    if re.search(query, model.get_property('title'), re.IGNORECASE):
+                        ids_to_show.append(song_id)
+        else:
+            ids_to_show = self.song_ids
+
+        ids_to_show = ids_to_show[:self.offset+30]
+
+        for row in list(self.list_el.list_el) + list(self.wrapbox_el):
+            GLib.idle_add(row.set_visible, row.id in ids_to_show)
+            if row.id in ids_to_show:
+                ids_to_show.remove(row.id)
+
+        for song_id in ids_to_show:
+            GLib.idle_add(self.list_el.list_el.append, SongRow(song_id))
+            GLib.idle_add(self.wrapbox_el.append, SongButton(song_id))
+        self.offset += 30
         GLib.idle_add(self.update_visibility)
+        self.searching = False
+
+    def check_scrollbar(self, adjustment):
+        if adjustment.get_upper() <= adjustment.get_page_size():
+            threading.Thread(target=self.search).start()
 
     def reset(self):
+        self.offset = 0
         self.list_el.list_el.remove_all()
         for el in list(self.wrapbox_el):
             self.wrapbox_el.remove(el)
+        integration = get_current_integration()
+        self.song_ids = integration.getStarredSongs()
+
+    def reload(self):
+        GLib.idle_add(self.main_stack.set_visible_child_name, 'loading')
+        GLib.idle_add(self.reset)
+        GLib.idle_add(self.search)
 
     @Gtk.Template.Callback()
     def on_search(self, search_entry):
-        query = search_entry.get_text()
-        for child in list(self.list_el.list_el) + list(self.wrapbox_el):
-            child.set_visible(child.get_name() != 'GtkListBoxRow' and re.search(query, child.get_name(), re.IGNORECASE))
-        GLib.idle_add(self.update_visibility)
+        self.offset = 0
+        threading.Thread(target=self.search, daemon=True).start()
+
+    @Gtk.Template.Callback()
+    def scroll_edge_reached(self, scrolledwindow, pos):
+        if pos == Gtk.PositionType.BOTTOM and self.offset < len(self.song_ids):
+            threading.Thread(target=self.search, daemon=True).start()
 
     def update_visibility(self):
         for row in list(self.list_el.list_el):
